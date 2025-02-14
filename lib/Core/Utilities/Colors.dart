cee48a345d43c4e717e0802fb2588a03aa3210b9
joeshaker <youssefshaker296@gmail.com>
2025-02-14 14:10:30 +0200

    Add_SignOutButtonColor

--- lib/Core/Utilities/Colors.dart
+++ lib/Core/Utilities/Colors.dart
@@ -5,3 +5,4 @@ import 'package:flutter/material.dart';
 Color background=Color(0xFF161667);
 Color backgroundTextForm=Color(0xFF25254A);
 Color loginButton= Colors.blue[900]!;
+Color signOutButton= Colors.blue[900]!;
